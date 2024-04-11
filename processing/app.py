446aad6c7bbb280431ba81fc22608a09d4807d1d
phuongvi1512 <phuongvi1512@gmail.com>
2024-04-11 15:48:40 -0700

    fix typo in processing app.py

--- processing/app.py
+++ processing/app.py
@@ -236,7 +236,7 @@ app.add_api("openapi.yaml", base_path="/processing", strict_validation=True, val
 
 if "TARGET_ENV" not in os.environ or os.environ["TARGET_ENV"] != "test":
     CORS(app.app)
-    app.app.config['CORS_HEEADERS'] = 'Content-Type'
+    app.app.config['CORS_HEADERS'] = 'Content-Type'
 
 if __name__ == "__main__":
     if not os.path.exists("/data/stats.sqlite"):
