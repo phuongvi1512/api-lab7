b6cb164ed3fcac20d13bf1613f65ba11f494bae1
phuongvi1512 <phuongvi1512@gmail.com>
2024-04-11 16:43:29 -0700

    readd the target env conditional

--- event_logger/app.py
+++ event_logger/app.py
@@ -17,14 +17,14 @@ from base import Base
 from create_db import create_db
 from event_stats import EventStats
 
-# if "TARGER_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
-#     print("In Test Environment")
-#     app_conf_file = "/config/app_conf.yml"
-#     log_conf_file = "/config/log_conf.yml"
-# else:
-#     print("In Dev Environment")
-#     app_conf_file = "app_conf.yml"
-#     log_conf_file = "log_conf.yml"
+if "TARGER_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+    log_conf_file = "/config/log_conf.yml"
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+    log_conf_file = "log_conf.yml"
 
 #read from app_conf.yaml
 with open('app_conf.yml', 'r') as f:
