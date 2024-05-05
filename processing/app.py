b038e9df758a8da7b728b73d263ce4dcf4e4fb95
phuongvi1512 <phuongvi1512@gmail.com>
2024-05-05 13:27:01 -0700

    update processing app.py, comment temporarily threshold

--- processing/app.py
+++ processing/app.py
@@ -1,4 +1,7 @@
-import os, time, uuid, json
+import os,
+import time
+import uuid
+import json
 from datetime import datetime
 import requests
 import logging
@@ -10,10 +13,7 @@ import swagger_ui_bundle
 from apscheduler.schedulers.background import BackgroundScheduler
 import connexion
 from connexion import NoContent
-from connexion.middleware import MiddlewarePosition
 from pykafka import KafkaClient
-from pykafka.common import OffsetType
-from starlette.middleware.cors import CORSMiddleware
 from flask_cors import CORS
 from base import Base
 from sqlalchemy import create_engine
@@ -39,8 +39,9 @@ with open('log_conf.yml', 'r') as f:
     logging.config.dictConfig(log_config)
 logger = logging.getLogger('basicLogger')
 
-logger.info("App Conf file: %s" % app_conf_file)
-logger.info("Log Conf file: %s" % log_conf_file)
+
+logger.info(f"App Conf file: ${app_conf_file}")
+logger.info(f"Log Conf file: ${log_conf_file}")
 
 with open('app_conf.yml', 'r') as f: 
     app_config = yaml.safe_load(f.read())  
@@ -51,8 +52,8 @@ THRESHOLD = app_config['events']['default_threshold']
 MAX_RETRY_COUNT = app_config['retry']['max_count']
 SLEEP_TIME = app_config['retry']['sleep_time']
 
-kafka_hostname = "%s:%d"%(app_config['events']['hostname'],
-                    app_config['events']['port'])
+kafka_hostname = f"{app_config['events']['hostname']}:
+                    {app_config['events']['port']}"
 logger.info("try to connect to logger kafka")
 retry_count = 0
 #connect to Kafka and log if app successfully start
@@ -117,18 +118,13 @@ def populate_stats():
     #starting log msg
     logger.info(f"periodically updating stats at {datetime.now()}")
 
-    #read stats from sqlite database
-    conn =sqlite3.connect(f'{app_config["datastore"]["filename"]}')
-    c = conn.cursor()
+    #connect to the sqlite database
+    session = DB_SESSION()
+    #query the stats_file table to get the latest stats
+    stats = session.query(StatsFile).order_by(StatsFile.last_updated.desc()).first()
 
-    #execute query to return the most recent last_updated from stats_file
-    c.execute("SELECT * FROM stats_file ORDER BY last_updated DESC LIMIT 1")
-    #c.execute("SELECT last_updated FROM stats_file LIMIT 1") 
-    result = c.fetchone()
-
-    #if there is no result, then add the stats file
-    if result is None:
-        cur_stats = {
+    if stats is None:
+        cur_stats =  {
             "last_updated": "2024-01-01 23:07:07.912972",
             "num_reports": 0,
             "num_files": 0,
@@ -136,17 +132,7 @@ def populate_stats():
             "max_file_size": 0
         }
     else:
-        #if there is a result, then get the last_updated and add the stats file
-        cur_stats = {
-            "last_updated": result[5],
-            "num_reports": result[1],
-            "num_files": result[2],
-            "max_temp": result[3],
-            "max_file_size": result[4]
-        }
-
-    #close the connection
-    conn.close()
+        cur_stats = stats.to_dict()
 
     #get current datetime
     cur_time = str(datetime.now())
@@ -166,40 +152,40 @@ def populate_stats():
         cfile_info = cfile_body.json()
 
         #publish if over threshold
-        if len(report_info) >= THRESHOLD:
-            content = {
-                "event_id": f"{str(uuid.uuid4())}",
-                "timestamp": f"{datetime.now()}",
-                "event_type": "switch_report",
-                "anomaly_type": "exceed_threshold",
-                "description": f"over threshold for switch report, value detected {len(report_info)}, threshold {THRESHOLD} exceeded {len(report_info) - THRESHOLD}"
-            }
-            msg = {
-                "event_type": "switch_report",
-                "anomaly_type": "exceed_threshold",
-                # "datetime": datetime.now().strftime('%Y-%m-%dT%H:%M:%S'),
-                "payload": content
-            }
-            msg_str = json.dumps(msg)
-            log_producer.produce(str.encode(msg_str))
-        elif len(cfile_info) >= THRESHOLD:
-            content = {
-                "event_id": f"{str(uuid.uuid4())}",
-                "timestamp": f"{datetime.now()}",
-                "event_type": "configuration_file",
-                "anomaly_type": "exceed_threshold",
-                "description": f"over threshold for config file, value detected {len(cfile_info)}, threshold {THRESHOLD} exceeded {len(cfile) - THRESHOLD}"
-            }
-            msg = {
-                "event_type": "configuration_file",
-                "anomaly_type": "exceed_threshold",
-                # "datetime": datetime.now().strftime('%Y-%m-%dT%H:%M:%S'),
-                "payload": content
-            }
-            msg_str = json.dumps(msg)
-            log_producer.produce(str.encode(msg_str))
-        else:
-            pass
+        # if len(report_info) >= THRESHOLD:
+        #     content = {
+        #         "event_id": f"{str(uuid.uuid4())}",
+        #         "timestamp": f"{datetime.now()}",
+        #         "event_type": "switch_report",
+        #         "anomaly_type": "exceed_threshold",
+        #         "description": f"over threshold for switch report, value detected {len(report_info)}, threshold {THRESHOLD} exceeded {len(report_info) - THRESHOLD}"
+        #     }
+        #     msg = {
+        #         "event_type": "switch_report",
+        #         "anomaly_type": "exceed_threshold",
+        #         # "datetime": datetime.now().strftime('%Y-%m-%dT%H:%M:%S'),
+        #         "payload": content
+        #     }
+        #     msg_str = json.dumps(msg)
+        #     log_producer.produce(str.encode(msg_str))
+        # elif len(cfile_info) >= THRESHOLD:
+        #     content = {
+        #         "event_id": f"{str(uuid.uuid4())}",
+        #         "timestamp": f"{datetime.now()}",
+        #         "event_type": "configuration_file",
+        #         "anomaly_type": "exceed_threshold",
+        #         "description": f"over threshold for config file, value detected {len(cfile_info)}, threshold {THRESHOLD} exceeded {len(cfile) - THRESHOLD}"
+        #     }
+        #     msg = {
+        #         "event_type": "configuration_file",
+        #         "anomaly_type": "exceed_threshold",
+        #         # "datetime": datetime.now().strftime('%Y-%m-%dT%H:%M:%S'),
+        #         "payload": content
+        #     }
+        #     msg_str = json.dumps(msg)
+        #     log_producer.produce(str.encode(msg_str))
+        # else:
+        #     pass
  
         #log msg number of events received
         logger.info(f"received {len(report_info)} reports and {len(cfile_info)} cfiles at {datetime.now()}")
@@ -221,14 +207,16 @@ def populate_stats():
 
         #write updated stats to sqlite database
         try:
-            conn = sqlite3.connect(f'{app_config["datastore"]["filename"]}')
-            c = conn.cursor()
-            c.execute("INSERT INTO stats_file (num_reports, num_files, max_temp, max_file_size, last_updated) VALUES (?, ?, ?, ?, ?)", (num_reports, num_files, max_temp, max_file_size, cur_time))
-            conn.commit()
-            conn.close()
-        except Exception as e:  
-            logger.error(f"failed to insert updated stats to the sqlite database at {datetime.now()} :%s", e)
-            return NoContent, 404
+            stats_record = StatsFile(num_reports=num_reports,
+                                     num_files=num_files,
+                                     max_temp=max_temp,
+                                     max_file_size=max_file_size,
+                                     last_updated=cur_time)
+            session.add(stats_record)
+            session.commit()
+            session.close()
+        except Exception as e:
+            logger.error(f"failed to insert updated stats to the sqlite database at {datetime.now()}. error is {e}")
 
         #ending log msg
         logger.info(f"finished updating stats at {datetime.now()}")
@@ -249,12 +237,6 @@ def init_scheduler():
 
 app = connexion.FlaskApp(__name__, specification_dir='')
 app.add_api("openapi.yaml", base_path="/processing", strict_validation=True, validate_responses=True)
-# app.add_middleware( CORSMiddleware, 
-#                    position=MiddlewarePosition.BEFORE_EXCEPTION, 
-#                    allow_origins=["*"], 
-#                    allow_credentials=True, 
-#                    allow_methods=["*"], 
-#                    allow_headers=["*"], )
 
 if "TARGET_ENV" not in os.environ or os.environ["TARGET_ENV"] != "test":
     CORS(app.app)
